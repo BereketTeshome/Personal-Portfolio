1599b6d2841b22c8d79f9138e2c406227f963c15
Bereket <bereketteshome685@gmail.com>
2023-09-28 02:14:47 -0700

    portfolio section updated

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,6 +3,7 @@ import './portfolio.css'
 import IMG2 from '../../assets/recipeHome.PNG'
 import IMG3 from '../../assets/ethioJobHome.PNG'
 import IMG4 from '../../assets/DreamHome.PNG'
+import IMG5 from '../../assets/AfroStore.PNG'
 
 
 const Portfolio = () => {
@@ -45,7 +46,19 @@ const Portfolio = () => {
 
             <div className="portfolio__item-cta">
               <a href="https://github.com/BereketTeshome/Ethio-Jobs.git" className='btn' target={'_blank'} rel="noreferrer">Github</a>
-              <a href="https://www.upwork.com/freelancers/~013c18cc64d84ad32b?p=1688500293268570112" className='btn btn-primary' target={'_blank'} rel="noreferrer">Video Demo</a>
+              <a href="https://vimeo.com/869062148?share=copy" className='btn btn-primary' target={'_blank'} rel="noreferrer">Video Demo</a>
+            </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="portfolio" />
+          </div>
+            <h3>Afro Store</h3>
+
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/BereketTeshome/Afro-Store.git" className='btn' target={'_blank'} rel="noreferrer">Github</a>
+              <a href="https://vimeo.com/869063394?share=copy" className='btn btn-primary' target={'_blank'} rel="noreferrer">Video Demo</a>
             </div>
         </article>
 
